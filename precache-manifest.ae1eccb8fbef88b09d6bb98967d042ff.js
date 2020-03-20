68a98a3adc75577df24351343f70245c1745aa0c
DzmitryKas <kasperovich_dima@mail.ru>
2020-03-20 15:46:04 +0300

    Updates

---
+++ precache-manifest.ae1eccb8fbef88b09d6bb98967d042ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bcb35f10f6d107245e41ef7661198ab",
+    "url": "/DzmitryKas/React_Application-/delta-stream/index.html"
+  },
+  {
+    "revision": "2e05b9a2e83bc17a6a5d",
+    "url": "/DzmitryKas/React_Application-/delta-stream/static/css/main.1baf450e.chunk.css"
+  },
+  {
+    "revision": "ab46c48b9305c9a93fd4",
+    "url": "/DzmitryKas/React_Application-/delta-stream/static/js/2.7b37fbf5.chunk.js"
+  },
+  {
+    "revision": "2e05b9a2e83bc17a6a5d",
+    "url": "/DzmitryKas/React_Application-/delta-stream/static/js/main.0e110927.chunk.js"
+  },
+  {
+    "revision": "ba78834892489e28b5d0",
+    "url": "/DzmitryKas/React_Application-/delta-stream/static/js/runtime-main.6b0f2717.js"
+  }
+]);
